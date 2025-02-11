02d8c7e95c56b48061bfcd790fd8bbe5af74487b
Mahan <khalili1388mahan@gmail.com>
2025-02-11 11:40:15 +0330

    Use internal icons instead saved files
    change lock_checkbox to pin_checkbox
    change freeze_checkbox to lock_checkbox
    use Button class for emission_speed_icon
    use editor theme icons for buttons
    modify code for Button class for emission_speed_icon
    change _on_lock_checkbox_toggled to _on_pin_checkbox_toggled and reconnect it
    change _on_freeze_checkbox_toggled to _on_lock_checkbox_toggled and reconnect it
    change node names with new names and connect those nodes to export variables
    remove icons from nodes
    update tooltips
    remove icon files (and .import)

--- addons/signal_lens/editor/signal_lens_editor_panel.gd
+++ addons/signal_lens/editor/signal_lens_editor_panel.gd
@@ -48,10 +48,18 @@ var pulsing_connections: Array = []
 @export var refresh_button: Button 
 @export var clear_button: Button
 @export var inactive_text: Label
-@export var lock_checkbox: CheckButton 
-@export var freeze_checkbox: CheckButton
+@export var pin_checkbox: CheckButton 
+@export var lock_checkbox: CheckButton
 @export var emission_speed_slider: Slider
-@export var emission_speed_icon: TextureRect
+@export var emission_speed_icon: Button
+
+## Initialize panel: Load icons
+func _ready() -> void:
+	clear_button.icon = EditorInterface.get_base_control().get_theme_icon("Clear", "EditorIcons")
+	refresh_button.icon = EditorInterface.get_base_control().get_theme_icon("Reload", "EditorIcons")
+	pin_checkbox.icon = EditorInterface.get_base_control().get_theme_icon("Pin", "EditorIcons")
+	lock_checkbox.icon = EditorInterface.get_base_control().get_theme_icon("Lock", "EditorIcons")
+	emission_speed_icon.icon = EditorInterface.get_base_control().get_theme_icon("Timer", "EditorIcons")
 
 ## Requests inspection of [param current_node] in remote scene
 func request_node_data():
@@ -65,25 +73,25 @@ func receive_node_data(data: Array):
 ## Sets up editor on project play
 func start_session():
 	clear_graph()
+	pin_checkbox.button_pressed = false
 	lock_checkbox.button_pressed = false
-	freeze_checkbox.button_pressed = false
 	emission_speed_slider.editable = true
-	emission_speed_icon.modulate = Color(emission_speed_icon.modulate, 1.0)
+	emission_speed_icon.disabled = false
 	node_path_line_edit.placeholder_text = TUTORIAL_TEXT
 	inactive_text.hide()
 
 ## Cleans up editor on project stop
 func stop_session():
 	clear_graph()
+	pin_checkbox.disabled = true
 	lock_checkbox.disabled = true
-	freeze_checkbox.disabled = true
 	refresh_button.disabled = true
 	clear_button.disabled = true
 	emission_speed_slider.editable = false
-	emission_speed_icon.modulate = Color(emission_speed_icon.modulate, 0.35)
+	emission_speed_icon.disabled = true
 	node_path_line_edit.text = ""
+	pin_checkbox.button_pressed = false
 	lock_checkbox.button_pressed = false
-	freeze_checkbox.button_pressed = false
 	inactive_text.show()
 
 ## Assigns a [param target_node] to internal member [param current_node]
@@ -205,13 +213,13 @@ func draw_node_data(data: Array):
 	# in case the buttons are disabled, they are enabled again
 	if clear_button.disabled:
 		clear_button.disabled = false
+	if pin_checkbox.disabled:
+		pin_checkbox.disabled = false
 	if lock_checkbox.disabled:
 		lock_checkbox.disabled = false
-	if freeze_checkbox.disabled:
-		freeze_checkbox.disabled = false
 	if emission_speed_slider.editable:
 		emission_speed_slider.editable = true
-		emission_speed_icon.modulate = Color(emission_speed_icon.modulate, 1.0)
+		emission_speed_icon.disabled = false
 
 func create_node(node_name: String, title_appendix: String = "") -> SignalLensGraphNode:
 	var new_node = SignalLensGraphNode.new()
@@ -326,13 +334,13 @@ func _on_clear_button_pressed() -> void:
 func _on_repo_button_pressed() -> void:
 	OS.shell_open("https://github.com/yannlemos/signal-lens")
 
-func _on_lock_checkbox_toggled(toggled_on: bool) -> void:
+func _on_pin_checkbox_toggled(toggled_on: bool) -> void:
 	block_new_inspections = toggled_on
 
 func _on_emission_speed_slider_value_changed(value: float) -> void:
 	emission_speed_multiplier = value
 
-func _on_freeze_checkbox_toggled(toggled_on: bool) -> void:
+func _on_lock_checkbox_toggled(toggled_on: bool) -> void:
 	if toggled_on:
 		freeze_signal_emissions()
 	else:
